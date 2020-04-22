17b2956028503147f8bdf90c99b3e1b715f84b87
jeffreygatbonton <jeff@creationcrate.com>
2020-04-22 12:09:49 -0400

    set up yoga and prisma, hooked up query and mutation files

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO: check if they are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({ // ctx.db.mutatuion returns a PROMISE, so we ASYNC it
+      data: {
+        ...args // same as individually pulling items from args
+      }
+    }, info)
+
+    console.log(item);
+    return item
+  }
+  // createDog(parent, args, ctx, info) {
+  //   // create a dog
+  //   global.dogs = global.dogs || []
+  //   const newDog = { name: args.name }
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
+
+module.exports = Mutations;
